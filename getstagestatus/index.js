94b8aa8fba9febb7d5c4c4b9f907cc14e3b7dc2f
Jakub Linhart <jakub.linhart@gmail.com>
2024-07-18 14:42:06 +0200

    list variables

--- getstagestatus/index.js
+++ getstagestatus/index.js
@@ -2,6 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const tl = require("azure-pipelines-task-lib/task");
 async function run() {
+    const variables = tl.getVariables()
+        .filter(x => !x.secret);
     try {
         const inputString = tl.getInput('samplestring', true);
         if (inputString == 'bad') {
@@ -9,6 +11,7 @@ async function run() {
             return;
         }
         console.log('Hello', inputString);
+        console.log(JSON.stringify(variables));
     }
     catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message);
--- getstagestatus/index.ts
+++ getstagestatus/index.ts
@@ -1,13 +1,17 @@
 import tl = require('azure-pipelines-task-lib/task');
 
- async function run() {
-     try {
+async function run() {
+    const variables = tl.getVariables()
+        .filter(x => !x.secret)
+    
+    try {
          const inputString: string | undefined = tl.getInput('samplestring', true);
          if (inputString == 'bad') {
              tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
              return;
          }
          console.log('Hello', inputString);
+         console.log(JSON.stringify(variables))
      }
      catch (err:any) {
          tl.setResult(tl.TaskResult.Failed, err.message);
